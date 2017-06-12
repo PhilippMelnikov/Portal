9bf5ce27fe96f0256aeaaaa9a62f9b7c6f7b61cd
Philipp Melnikov <fmelnikov123@gmail.com>
2017-06-12 18:20:08 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -38,21 +38,60 @@ $(document).ready(function () {
         else{
             $(this).find('.fa-angle-double-down').removeClass('arrow-up');
         }
-        
+
     })
 
-    if($(this).width()<768)
-        {
-            $('.email-input').attr('placeholder', 'enter your email');
+    // anchors
+      $('.anchor').click(function () {
+         var hrefName = $(this).attr('href');
+         var timeToScroll = Math.abs($(window).scrollTop() - $(hrefName).offset().top) / 3;
+       $('html, body').animate({
+             scrollTop: $(hrefName).offset().top - 60
+         }, timeToScroll);
+     });
+
+    // swiper
+      var mySwiper = new Swiper ('.swiper2', {
+        // Optional parameters
+        loop: true,
+
+        // If we need pagination
+        pagination: '.swiper-pagination',
+        centeredSlides: true,
+        slidesPerView: 1,
+        autoplay: 3000
+
+      })
+      // floating header
+      $(this).scroll(function() {
+          // console.log($(this).scrollTop());
+        if($(this).scrollTop() >= $('.counterfeit-1').offset().top){
+          $('.main-menu').addClass('floating-menu');
+          $('.counterfeit-1').css('height', $('.main-menu').height());
+        }else{
+          $('.main-menu').removeClass('floating-menu');
+          $('.counterfeit-1').css('height', 0);
         }
 
-    $(window).resize(function(){
-        if($(this).width()<768)
-        {
-            $('.email-input').attr('placeholder', 'enter your email');
+        if($(this).scrollTop() >= $('.counterfeit-2').offset().top){
+          $('.accordion-menu').addClass('floating-menu');
+          $('.counterfeit-2').css('height', $('.accordion-menu').height());
+        }else{
+           console.log($(this).scrollTop());
+          $('.accordion-menu').removeClass('floating-menu');
+          $('.counterfeit-2').css('height', 0);
+        }
+      })
+      if($(window).width()>768){
+        $('.accordion-menu-container').css('display', 'none');
+      }
+      $(window).resize(function(){
+        if($(window).width()>768){
+          $('.accordion-menu-container').css('display', 'none');
         }
         else{
-            $('.email-input').attr('placeholder', 'enter your email for our newsletter');
+          $('.accordion-menu-container').css('display', 'block');
         }
-    })
+      })
+
 })
